5d3c8d319667ba1e7da55acdccefaac74d0000b1
MuzammilBC <mbchandashah@gmail.com>
2023-05-25 15:41:28 +0530

    second commit

---
+++ src/main/java/com/rakuten/gitspringmaven/Employee.java
@@ -0,0 +1,13 @@
+package com.rakuten.gitspringmaven;
+
+public class Employee {
+	private String name;
+	
+	public Employee(String name) {
+		this.name=name;
+	}
+
+	public void displayname() {
+		System.out.println("Welecome to Maven :"+this.name);
+	}
+}
